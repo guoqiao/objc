6d0a73a1dca1e53d858ebfd1e5dfff271f1dc96a
guoqiao <guoqiao@gmail.com>
2014-05-15 11:56:20 +1200

    use property

--- 8-AddressBook/AddressCard.h
+++ 8-AddressBook/AddressCard.h
@@ -1,10 +1,11 @@
 #import <Foundation/Foundation.h>
 
 @interface AddressCard: NSObject
--(void) setName: (NSString*) name;
--(void) setEmail: (NSString*) email;
--(NSString*) name;
--(NSString*) email;
+//-(void) setName: (NSString*) name;
+//-(void) setEmail: (NSString*) email;
+//-(NSString*) name;
+//-(NSString*) email;
+@property (copy, nonatomic) NSString* name, *email;
 -(void) print;
 @end
 
--- 8-AddressBook/AddressCard.m
+++ 8-AddressBook/AddressCard.m
@@ -6,32 +6,34 @@
     NSString* _email;
 }
 
--(void) setName: (NSString*) name
-{
-    _name = [NSString stringWithString: name];
-}
+//-(void) setName: (NSString*) name
+//{
+    //_name = [NSString stringWithString: name];
+//}
 
--(void) setEmail: (NSString*) email
-{
-    _email = [NSString stringWithString: email];
-}
+//-(void) setEmail: (NSString*) email
+//{
+    //_email = [NSString stringWithString: email];
+//}
 
--(NSString*) name
-{
-    return _name;
-}
+//-(NSString*) name
+//{
+    //return _name;
+//}
 
--(NSString*) email
-{
-    return _email;
-}
+//-(NSString*) email
+//{
+    //return _email;
+//}
+
+@synthesize name, email;
 
 -(void) print
 {
     NSLog(@"==============================");
     NSLog(@"|                            |");
-    NSLog(@"|  %-24s  |",[_name UTF8String]);
-    NSLog(@"|  %-24s  |",[_email UTF8String]);
+    NSLog(@"|  %-24s  |",[name UTF8String]);
+    NSLog(@"|  %-24s  |",[email UTF8String]);
     NSLog(@"|                            |");
     NSLog(@"|                            |");
     NSLog(@"|                            |");
