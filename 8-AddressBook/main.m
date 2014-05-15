71bdf0ccbe9fdb6ace7ce25cbb6a6854898988bc
guoqiao <guoqiao@gmail.com>
2014-05-15 12:01:06 +1200

    use dot

--- 8-AddressBook/main.m
+++ 8-AddressBook/main.m
@@ -6,8 +6,10 @@ int main(int argc, const char * argv[])
         NSString* name = @"joe";
         NSString* email = @"guoqiao@gmail.com";
         AddressCard* c = [AddressCard new];
-        [c setName: name];
-        [c setEmail: email];
+        //[c setName: name];
+        //[c setEmail: email];
+        c.name = name;
+        c.email = email;
         [c print];
     }
     return 0;
